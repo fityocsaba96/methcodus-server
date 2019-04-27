fc4f25b54886699438d888b62b481d040b5a82dd
fityocsaba96 <fityocsaba96@inf.elte.hu>
2019-04-27 13:32:57 +0200

    feat(testing): solution importer for javascript json tester

--- testing/javascript/jasmine-test-code-generator.js
+++ testing/javascript/jasmine-test-code-generator.js
@@ -3,6 +3,21 @@ module.exports = class JasmineTestCodeGenerator {
 
   generate(testJson) {
     this._testJson = testJson;
-    return '';
+    this._testCode = '';
+    addSolutionImporter();
+    return this._testCode;
+  }
+
+  addSolutionImporter() {
+    this._testCode += `{
+      const imports = require('.');
+      const importToGlobal = ([name, imported]) =>
+        (global[typeof imported === 'function' ? imported.name : name] = imported);
+      if (typeof imports === 'object') {
+        Object.entries(imports).forEach(importToGlobal);
+      } else {
+        importToGlobal(['default', imports]);
+      }
+    }`;
   }
 };
