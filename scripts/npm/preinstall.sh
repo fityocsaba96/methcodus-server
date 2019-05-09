1e3949e6d6af63199127efc720313fb797bbdd69
fityocsaba96 <fityocsaba96@inf.elte.hu>
2019-05-09 19:22:15 +0200

    fix(scripts): relative directories to work outside docker

--- scripts/npm/preinstall.sh
+++ scripts/npm/preinstall.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd /app/testing/javascript
+PROJECT_DIR=$(pwd)
+
+cd "${PROJECT_DIR}/testing/javascript"
 npm install --prefix . jasmine@3.4.0 @babel/core@7.4.4 @babel/register@7.4.4 @babel/preset-env@7.4.4
 chmod o+w codes
 
-cd /app/testing/java
+cd "${PROJECT_DIR}/testing/java"
 MAVEN_DOWNLOAD_URL="http://search.maven.org/remotecontent?filepath="
 curl -sL -o junit.jar "${MAVEN_DOWNLOAD_URL}org/junit/platform/junit-platform-console-standalone/1.4.2/junit-platform-console-standalone-1.4.2.jar"
 curl -sL -o json.jar "${MAVEN_DOWNLOAD_URL}org/json/json/20180813/json-20180813.jar"
