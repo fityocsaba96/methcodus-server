e213109de7e160593d6b99db43e30e1febaeccf9
fityocsaba96 <fcsabika@gmail.com>
2019-12-13 10:43:11 +0100

    feat(app): add cors support for allowed origins

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app/app.module';
 bootstrap();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: process.env.ALLOWED_ORIGIN },
+  });
   await app.listen(process.env.PORT);
 }
