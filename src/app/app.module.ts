bcf2d80150f0a93447ec38da10716dd5f265bed0
fityocsaba96 <fcsabika@gmail.com>
2020-05-22 15:22:01 +0200

    feat(db): add mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TesterModule } from './tester/tester.module';
 
 @Module({
-  imports: [TesterModule],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI, { useNewUrlParser: true }), TesterModule],
 })
 export class AppModule {}
