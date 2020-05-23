da6ce5acea86597571f3484f56f4388d814b4e7d
fityocsaba96 <fcsabika@gmail.com>
2020-05-23 22:36:51 +0200

    feat(auth): add jwt auth guard for websocket

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
+
+@Injectable()
+export class WebSocketJwtAuthGuard implements CanActivate {
+  public async canActivate(context: ExecutionContext) {
+    const webSocketContext = context.switchToWs();
+    const client = webSocketContext.getClient();
+    if (client.authenticated === true) {
+      return true;
+    }
+    const data = webSocketContext.getData();
+    if (typeof data !== 'string' || data.substring(0, 6) !== 'Bearer') {
+      return false;
+    }
+    const jwtToken = data.substring(7);
+    let payload: any;
+    try {
+      payload = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY, { ignoreExpiration: true });
+    } catch {
+      return false;
+    }
+    client.user = { _id: payload.sub, userName: payload.username };
+    client.authenticated = true;
+    return true;
+  }
+}
--- src/pair-programming/pair-programming.gateway.ts
+++ src/pair-programming/pair-programming.gateway.ts
@@ -1,7 +1,15 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage } from '@nestjs/websockets';
 import { PairProgrammingRequestService } from 'src/pair-programming-request/pair-programming-request.service';
+import { UseGuards } from '@nestjs/common';
+import { WebSocketJwtAuthGuard } from 'src/auth/auth.guard';
 
 @WebSocketGateway({ namespace: 'pair-programming' })
 export class PairProgrammingGateway {
   constructor(private readonly pairProgrammingRequestService: PairProgrammingRequestService) {}
+
+  @UseGuards(WebSocketJwtAuthGuard)
+  @SubscribeMessage('authenticate')
+  public async onAuthenticate(): Promise<boolean> {
+    return true;
+  }
 }
