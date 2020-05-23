a261787e10a9bc96fb7bbcbe501c721e423bed87
fityocsaba96 <fcsabika@gmail.com>
2020-05-23 15:55:44 +0200

    feat(auth): add login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  public async login(@Request() request): Promise<{ access_token: string }> {
+    return this.authService.generateJWT(request.user);
+  }
 }
