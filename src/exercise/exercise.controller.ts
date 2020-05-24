15ce585c92aa3a9b6804f4ea83f923350ad20802
fityocsaba96 <fcsabika@gmail.com>
2020-05-24 13:15:03 +0200

    feat(exercise): add get own exercises endpoint

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { Request as ExpressRequest } from 'express';
+import { Exercise } from './exercise.schema';
+import { pipe, pick, map, assoc } from 'ramda';
 
 @Controller('exercises')
 export class ExerciseController {
   constructor(private readonly exerciseService: ExerciseService) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  public async getMine(@Request() request: ExpressRequest): Promise<Partial<Exercise>[]> {
+    const user = request.user as any;
+    return map(pipe(pick(['_id', 'createdAt', 'name']), assoc('createdBy', { userName: user.userName })))(
+      await this.exerciseService.findAllByCreatedBy(user._id),
+    );
+  }
 }
--- src/exercise/exercise.service.ts
+++ src/exercise/exercise.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Exercise } from './exercise.schema';
 
 @Injectable()
 export class ExerciseService {
   constructor(@InjectModel(Exercise.name) private readonly exerciseModel: Model<Exercise>) {}
+
+  public async findAllByCreatedBy(createdBy: string): Promise<Exercise[]> {
+    return this.exerciseModel.find({ createdBy: Types.ObjectId(createdBy) as any });
+  }
 }
